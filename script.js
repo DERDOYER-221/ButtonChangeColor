6a8e0775e2cbae2da413f6f127f0a32b10297de5
DERDOYER-221 <58949396+DERDOYER-221@users.noreply.github.com>
2024-12-06 06:33:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const btnChangeWindowSize = document.getElementById('btn-change-window-size');
+const btnChangeWindowSizeSpan = document.getElementsByClassName('text')[0];
+const windowChangeSize = document.getElementsByClassName('window')[0];
+const colorKey = document.getElementsByClassName('color-key')[0];
+const colorValue = document.getElementsByClassName('color-value')[0];
+const colorText = document.getElementsByClassName('color-text')[0];
+
+btnChangeWindowSize.addEventListener('mouseenter', function() {
+	btnChangeWindowSizeSpan.style.color = 'white';
+  btnChangeWindowSizeSpan.style.fontStyle = 'italic';
+  if (windowChangeSize.classList.contains('calm-blue')) {
+  	btnChangeWindowSizeSpan.style.textShadow = '0.5px 0.5px #F3D2D3';
+    colorValue.innerHTML = 'Calm Blue';
+    colorText.style.color = 'white';
+  } else {
+  	btnChangeWindowSizeSpan.style.textShadow = '0.5px 0.5px #4895BF';
+    colorValue.innerHTML = 'Light Pink';
+    colorText.style.color = 'black';
+  }
+})
+
+btnChangeWindowSize.addEventListener('mouseleave', function() {
+	btnChangeWindowSizeSpan.style.color = 'white';
+  btnChangeWindowSizeSpan.style.textShadow = 'none';
+  btnChangeWindowSizeSpan.style.fontStyle = 'none';
+})
+
+btnChangeWindowSize.addEventListener('click', function() {
+	windowChangeSize.classList.toggle('calm-blue');
+})
